14bf6c9826378f87e16e1459225292068e004c45
aishu-suresh <aiswaryasuresh53341@gmail.com>
2025-02-21 11:18:52 +0530

    Add files via upload

---
+++ Rollno.java
@@ -0,0 +1,12 @@
+public class Rollno
+{
+    void getinput(int b)
+    {
+        System.out.println("B="+b);
+    }
+    public static void main(String args[])
+    {
+        Rollno ob = new Rollno();
+        ob.getinput(4);
+    }
+}

11c2fc1c33ff589321e38561a247b75721225086
aishu-suresh <aiswaryasuresh53341@gmail.com>
2025-02-21 11:10:27 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,14 @@
+public class Course
+{
+        public static void main(String args[])
+      {
+        int rollno=4;
+        String Course="MCA";
+        System.out.println("Course:"+Course);
+        System.out.println("rollno:"+rollno);
+
+      }  
+
+        
+
+}

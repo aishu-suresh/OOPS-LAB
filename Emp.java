6cb152701567fd0ef25aaa2cded4634de03a9882
aishu-suresh <aiswaryasuresh53341@gmail.com>
2025-02-21 11:13:50 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,12 @@
+public class Emp
+{
+        void displaymsg()
+    {
+        System.out.println("Employees");
+    }
+        public static void main(String args[])
+    {
+        Emp ob = new Emp();
+        ob.displaymsg();
+    }
+}
